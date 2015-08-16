08538a72ba6f69513ac6952b03926245014d2311
Jeff Meisel <jeffreymeisel@gmail.com>
2015-08-16 10:01:00 -0500

    Initial Commit

---
+++ acsvariables.js
@@ -0,0 +1,98 @@
+    var data = [
+    {
+        type: "social",
+        name: "Ancestry",
+        links: [{
+            url: "https://github.com/dssg/innovation-ecosystems",
+            title: "List of Variables"
+        }]
+    },
+        {
+        type: "housing",
+        name: "Bedrooms",
+        links: [{
+            url: "https://github.com/dssg/innovation-ecosystems",
+            title: "List of Variables"
+        }]
+    },
+    {
+        type: "economic",
+        name: "Class of Worker",
+        links: [{
+            url: "https://github.com/open-austin/Austin_Parks_Equity",
+            title: "List of Variables"
+        }]
+    },
+    {
+        type: "economic",
+        name: "Commuting to Work",
+        links: [{
+            url: "https://github.com/open-austin/Austin_Parks_Equity",
+            title: "List of Variables"
+        }]
+    },
+    {
+        type: "social",
+        name: "Disability Status",
+        links: [{
+            url: "https://github.com/dssg/innovation-ecosystems",
+            title: "List of Variables"
+        }]
+    },
+    {
+        type: "social",
+        name: "Educational Attainment",
+        links: [{
+            url: "https://github.com/dssg/innovation-ecosystems",
+            title: "List of Variables"
+        }]
+    },
+    {
+        type: "economic",
+        name: "Employment Status",
+        links: [{
+            url: "https://github.com/open-austin/Austin_Parks_Equity",
+            title: "List of Variables"
+        }]
+    },
+    {
+        type: "housing",
+        name: "Gross Rent",
+        links: [{
+            url: "https://github.com/dssg/innovation-ecosystems",
+            title: "List of Variables"
+        }]
+    },
+    {
+        type: "demographic",
+        name: "Hispanic of Latino Race",
+        links: [{
+            url: "https://github.com/dssg/innovation-ecosystems",
+            title: "List of Variables"
+        }]
+    },
+    {
+        type: "demographic",
+        name: "Race",
+        links: [{
+            url: "https://github.com/dssg/innovation-ecosystems",
+            title: "List of Variables"
+        }]
+    },
+
+    {
+        type: "demographic",
+        name: "Sex and Age",
+        links: [{
+            url: "https://github.com/dssg/innovation-ecosystems",
+            title: "List of Variables"
+        }]
+    },
+        {
+        type: "housing",
+        name: "Housing Occupancy",
+        links: [{
+            url: "https://github.com/dssg/innovation-ecosystems",
+            title: "List of Variables"
+        }]
+    }];
